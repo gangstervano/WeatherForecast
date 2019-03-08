691483103b8ea9b82ef6f1362e3a70ceb74ed3b6
Ivan Koshkidko <22711722+gangstervano@users.noreply.github.com>
2019-03-08 21:31:32 +0300

    Pre alpha

---
+++ WeatherForecast/Internet.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.IO;
+
+namespace WeatherForecast
+{
+    class Internet
+    {
+        static public string GetHtmlPage(string url)
+        {
+            string HtmlText = string.Empty;
+            HttpWebRequest myHttwebrequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            HttpWebResponse myHttpWebresponse = (HttpWebResponse)myHttwebrequest.GetResponse();
+            StreamReader strm = new StreamReader(myHttpWebresponse.GetResponseStream());
+            HtmlText = strm.ReadToEnd();
+            return HtmlText;
+        }
+    }
+}
---
+++ WeatherForecast/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WeatherForecast
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter outputFile = new StreamWriter("GisMeteo.txt");
+            string HtmlText = Internet.GetHtmlPage("https://www.gismeteo.ru/city/daily/4327/");
+            outputFile.WriteLine(HtmlText);
+        }
+    }
+}
